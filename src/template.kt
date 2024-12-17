4105915ad5e4c593ea4dcbdc2cd5cabb276cf63d
Abel Tay <15604207+abeltay@users.noreply.github.com>
2024-12-17 22:34:43 +0800

    Wrap with converter

--- src/template.kt
+++ src/template.kt
@@ -1,24 +1,28 @@
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun parse(input: List<String>): List<MutableList<Char>> {
+        return input.map { it.toMutableList() }
+    }
+
+    fun part1(input: List<MutableList<Char>>): Int {
         return input.size
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<MutableList<Char>>): Int {
         return input.size
     }
 
     // Read a large test input from the `src/Day{TEMPLATE}_test1.txt` file:
     val testInput1 = readInput("Day{TEMPLATE}_test1")
-    check(part1(testInput1) == 1)
-    // check(part2(testInput1) == 1)
+    check(part1(parse(testInput1)) == 1)
+    // check(part2(parse(testInput1)) == 1)
 
     // Read a large test input from the `src/Day{TEMPLATE}_test2.txt` file:
     // val testInput2 = readInput("Day{TEMPLATE}_test2")
-    // check(part1(testInput2) == 1)
+    // check(part1(parse(testInput2)) == 1)
 
     // Read the input from the `src/Day{TEMPLATE}.txt` file.
     val input = readInput("Day{TEMPLATE}")
     println("Running input")
-    part1(input).println()
-    part2(input).println()
+    part1(parse(input)).println()
+    part2(parse(input)).println()
 }
