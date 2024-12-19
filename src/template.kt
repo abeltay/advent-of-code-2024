df4ee0a6ab1cc5772c258d0089b0db82b21ebd1a
Abel Tay <15604207+abeltay@users.noreply.github.com>
2024-12-19 14:56:06 +0800

    Use Long by default and move parsing to wrap ReadInput
    Too many "too low" results

--- src/template.kt
+++ src/template.kt
@@ -1,28 +1,28 @@
 fun main() {
-    fun parse(input: List<String>): List<MutableList<Char>> {
-        return input.map { it.toMutableList() }
+    fun parse(input: List<String>): List<String> {
+        return input
     }
 
-    fun part1(input: List<MutableList<Char>>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        return input.size.toLong()
     }
 
-    fun part2(input: List<MutableList<Char>>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        return input.size.toLong()
     }
 
     // Read a large test input from the `src/Day{TEMPLATE}_test1.txt` file:
-    val testInput1 = readInput("Day{TEMPLATE}_test1")
-    check(part1(parse(testInput1)) == 1)
-    // check(part2(parse(testInput1)) == 1)
+    val testInput1 = parse(readInput("Day{TEMPLATE}_test1"))
+    check(part1(testInput1) == 0L)
+    // check(part2(parse(testInput1)) == 1L)
 
     // Read a large test input from the `src/Day{TEMPLATE}_test2.txt` file:
-    // val testInput2 = readInput("Day{TEMPLATE}_test2")
-    // check(part1(parse(testInput2)) == 1)
+    // val testInput2 = parse(readInput("Day{TEMPLATE}_test2"))
+    // check(part1(testInput2) == 1L)
 
     // Read the input from the `src/Day{TEMPLATE}.txt` file.
-    val input = readInput("Day{TEMPLATE}")
+    val input = parse(readInput("Day{TEMPLATE}"))
     println("Running input")
-    part1(parse(input)).println()
-    part2(parse(input)).println()
+    part1(input).println()
+    part2(input).println()
 }
